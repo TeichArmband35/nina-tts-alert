8c96b464f741755ca27ab323ca8749946b772be1
Pille <atomkraftstar35@gmail.com>
2026-08-13 22:13:17 +0200

    Update server.js

--- server/DownloadWebsite/server.js
+++ server/DownloadWebsite/server.js
@@ -22,9 +22,9 @@ var SeverityOfWarning = [];
 
 // CONFIG
 const tokens = [
-  "Insert here first encrypted password", // Token for ninaPush
-  "Insert here second encrypted password", // Token for ninaPush online server status
-  "Insert here third encrypted password", // Token for ninaPush Transcritpt etc.
+  "$2b$12$yIrEmqdgCr7QQZeCEb8j5O0pC38AWMBJsoSZdzAgpeYbwTijooz92", // Token for ninaPush
+  "$2b$12$WCNcVEZlU9YHCWvpfa.YeehMtL8bt5iEp88Nz2SBBtxKmKVSS1.rO", // Token for ninaPush online server status
+  "$2b$12$PyeMJ1x7XJLZgyD/5w7qxe5Pg/jyGUkv7Q9ZuADsf6JtNn6jYDWgW", // Token for ninaPush Transcritpt etc.
 ];
 // No config behind this point, keine config nach diesem punkt leeel
 
@@ -76,6 +76,7 @@ var ninattsserverStatusText = "Unknown Internal Server Error";
 
 
 app.get("/server/nina/audio/Warnungen", (req, res) => {
+  console.log("[INFO]: Get-Request: /server/nina/audio/Warnungen")
   var file = getFilePath();
   if (ninattsRunningBoolean) {
     SeverityOfWarning = severityToNumber(ninattsSeverity);
@@ -95,10 +96,13 @@ app.get("/server/nina/audio/Warnungen", (req, res) => {
 app.get("/server/nina/audio/Warnungen/Download", (req, res) => {
   var file = getFilePath();
 
+  if (file === "N/A") {
+    res.status(404).json("Keine Warnung gefunden");
+    return;
+  }
+
   res.download(file, (err) => {
-    if (file === "N/A") {
-      res.status(404).json("Keine Warnung gefunden");
-    } else if (err) {
+    if (err) {
       res.status(500).send(Error500);
       Errors.error.push(`${Error500} bei /downloadSeverExtrem`);
     }
@@ -223,5 +227,5 @@ app.get("/server/status", (req, res) => {
 
 
 // PORT
-app.listen(port, () => console.log(`SERVER: Belauscht Port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`SERVER: Belauscht Port ${port}`));
 console.log("CMDs f�r Server:", ServerCMDs);
