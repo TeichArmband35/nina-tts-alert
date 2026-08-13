6d756acbb7754e0e9c5af6a11593bfd2b8d90e6c
Pille <atomkraftstar35@gmail.com>
2026-08-13 22:09:41 +0200

    Update warnungstextmaker.js

--- server/warnungstextmaker.js
+++ server/warnungstextmaker.js
@@ -6,6 +6,7 @@ function WarnungsNarchichtErstellen(
     severity,
     data,
     instruction,
+    senderName,
 ) {
     var descriptionStringFiltered = data.info[0].description
         .replace(/(<br\s*\/?>)+/gi, " ")
@@ -13,33 +14,34 @@ function WarnungsNarchichtErstellen(
 
     const Anfang = "Warnung!";
     var Einleitung =
-        "Das Bundesamt für Bevölkerungsschutz und Katastrophenhilfe warnt vor dem Ereignis. " +
+        `Die zust�ndige Stelle ${senderName} warnt vor dem Ereignis. ` +
         headline;
     var beschreibung1 = [];
     var beschreibung2 = [];
     const beschreibung3 = "Die Beschreibung von der Warnung wird nun vorgelesen.";
     const beschreibung4 =
-        ". Falls Seh-renen, Warnung der Bevölkerung heulen, welcher ein Auf- und Abschwellender heulton ist, befolgen sie folgende Schritte:";
+        ". Falls Seh-renen, Warnung der Bev�lkerung heulen, welcher ein Auf- und Abschwellender heulton ist, befolgen sie folgende Schritte:";
     const beschreibung5 =
-        ". Suchen sie ein Gebäude auf. Schließen sie Fenster und Türen. Deaktivieren sie, falls möglich, alle Klimaanlagen und Lüftungen. Schalten sie Rundfunkgeräte ein. Beachten sie Meldungen von Warn-Apps. Befolgen sie Anweisungen der Behörden. Informieren sie ihre Nachbarn. Bitte benutzen sie nur Notrufleitungen für Notfälle.";
-    const ende = ". Diese Angaben sind ohne Gewähr. Das verwendete Sprach-Modell kann Telefonnummern, Uhrzeiten und Informationen falsch vorlesen. Bitte überprüfen Sie diese in der NINA App. Befolgen Sie ausschließlich offizielle Anweisungen der Behörden.";
-    const beschreibung6 = "Falls Seh-renen, Entwarnung heulen, welcher ein 60 sekündiger Dauerton ist, ist diese oder eine andere Warnung aufgehoben.";
+        ". Suchen sie ein Geb�ude auf. Schlie�en sie Fenster und T�ren. Deaktivieren sie, falls m�glich, alle Klimaanlagen und L�ftungen. Schalten sie Rundfunkger�te ein. Beachten sie Meldungen von Warn-Apps. Befolgen sie Anweisungen der Beh�rden. Informieren sie ihre Nachbarn. Bitte benutzen sie nur Notrufleitungen f�r Notf�lle.";
+    const ende = ". Diese Angaben sind ohne Gew�hr. Das verwendete Sprach-Modell kann Telefonnummern, Uhrzeiten und Informationen falsch vorlesen. Bitte �berpr�fen Sie diese in der NINA App. Befolgen Sie ausschlie�lich offizielle Anweisungen der Beh�rden.";
+    const beschreibung6 = "Falls Seh-renen, Entwarnung heulen, welcher ein 60 sek�ndiger Dauerton ist, ist diese oder eine andere Warnung aufgehoben.";
 
     if (severity == "Moderate") {
         beschreibung1 = ". Die Warnung hat den Schweregrad moderat.";
         beschreibung2 =
-            "Eine moderate Warnung bedeutet das die Gefahr Mäßig, potenziell schädlich, aber nicht lebensbedrohlich ist.";
+            "Eine moderate Warnung bedeutet das die Gefahr M��ig, potenziell sch�dlich, aber nicht lebensbedrohlich ist.";
     } else if (severity == "Severe") {
         beschreibung1 = ". Die Warnung hat den Schweregrad schwer.";
         beschreibung2 =
-            "Eine schwere Warnung bedeutet das die Gefahr sehr ernst ist und das schwere Schäden oder Verletzungen möglich sind.";
+            "Eine schwere Warnung bedeutet das die Gefahr sehr ernst ist und das schwere Sch�den oder Verletzungen m�glich sind.";
     } else if (severity == "Extreme") {
         beschreibung1 = ". Die Warnung hat den Schweregrad extrem.";
         beschreibung2 =
-            "Eine extreme Warnung bedeutet das die Gefahr Lebensbedrohlich oder katastrophal ist. Dies ist die höchste Gefahrenstufe.";
+            "Eine extreme Warnung bedeutet das die Gefahr Lebensbedrohlich oder katastrophal ist. Dies ist die h�chste Gefahrenstufe.";
     }
 
-    var beschreibung7 = `Falls es weitere zu ergreifende Maßnahmen bezüglich der Warnung gibt, werden diese nun vorgelesen. ${instruction}.`;
+    var beschreibung7 = `Falls es weitere zu ergreifende Ma�nahmen bez�glich der Warnung gibt, werden diese nun vorgelesen. ${instruction}.`;
+
 
      return [
          Anfang,
